3e31080f4f9ed54b537ef74e782d3909fe35c30d
kyle <sdfcvbjkl@gmail.com>
2021-12-14 15:31:10 +0800

    feat: get cart status from API

--- shop.js
+++ shop.js
@@ -4,9 +4,11 @@ const apiPath = 'kn99';
 
 const productList = document.querySelector('.productWrap');
 const productSelect = document.querySelector('.productSelect');
+const cartTable = document.querySelector('.shoppingCart-table');
 
 function init() {
     getProductList();
+    getCartList();
 }
 
 init();
@@ -17,7 +19,7 @@ function getProductList() {
         url: `${apiUrl}/api/livejs/v1/customer/${apiPath}/products`
     })
     .then(res => {
-        console.log(res.data);
+        // console.log(res.data);
         renderProductList(res.data.products);
         createProductSelect(res.data.products);
     })
@@ -41,10 +43,86 @@ function renderProductList(productData) {
         `;
     });
     productList.innerHTML = str;
+    bindAddCartEvent(productData);
 }
 
 function getFormatPrice(price) {
     return `NT$${price.toLocaleString('en-US')}`;
+}
+
+//TODO
+function bindAddCartEvent(productData) {
+    productList.addEventListener('click', e => {
+        let tar = e.target;
+        tar.classList.forEach(item => {
+            if (item === 'addCardBtn') {
+                console.log(tar.dataset.id);
+                // 加入購物車
+            }
+        });
+    })
+}
+
+function getCartList() {
+    axios({
+        method: 'get',
+        url: `${apiUrl}/api/livejs/v1/customer/${apiPath}/carts`
+    })
+    .then(res => {
+        // console.log(res.data);
+        renderCartList(res.data)
+    })
+    .catch(err => {
+        console.log(err.response);
+    })
+}
+
+function renderCartList(cartData) {
+    console.log(cartData);
+    let carts = cartData.carts;
+    // 建立購物車 HTML
+    let str = `
+        <tr>
+            <th width="40%">品項</th>
+            <th width="15%">單價</th>
+            <th width="15%">數量</th>
+            <th width="15%">金額</th>
+            <th width="15%"></th>
+        </tr>`;
+
+    carts.forEach(item => {
+        str += `<tr>
+                    <td>
+                        <div class="cardItem-title">
+                            <img src="${item.product.images}" alt="${item.title}">
+                            <p>${item.product.title}</p>
+                        </div>
+                    </td>
+                    <td>${getFormatPrice(item.product.price)}</td>
+                    <td>${item.quantity}</td>
+                    <td>${getFormatPrice(item.product.price * item.quantity)}</td>
+                    <td class="discardBtn" data-id=${item.id}>
+                        <a href="#" class="material-icons">
+                            clear
+                        </a>
+                    </td>
+                </tr>`;
+    });
+
+    str += `
+            <tr>
+                <td>
+                    <a href="#" class="discardAllBtn">刪除所有品項</a>
+                </td>
+                <td></td>
+                <td></td>
+                <td>
+                    <p>總金額</p>
+                </td>
+                <td>${getFormatPrice(cartData.finalTotal)}</td>
+            </tr>`;
+
+    cartTable.innerHTML = str;
 }
 
 function createProductSelect(productData) {
@@ -72,4 +150,16 @@ function bindSelectProductEvent(productData) {
       
       renderProductList(selectedProduct);
     });
-  }
+}
+
+//TODO
+function createCart(productId) {
+    // 送出購物車請求
+    axios({
+        method: post,
+        url: `${apiUrl}/api/livejs/v1/customer/${apiPath}/carts`
+    })
+
+    // 重新 render 購物車區塊
+
+}
